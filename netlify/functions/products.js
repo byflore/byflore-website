1f8bb84741361ca0b70c09beecc9326311b0da43
byflore <byflorestudio.1@gmail.com>
2026-05-11 14:50:49 +0800

    Update products.js

--- netlify/functions/products.js
+++ netlify/functions/products.js
@@ -1,39 +1,24 @@
-const https = require(“https”);
-
+var https = require("https");
 exports.handler = function(event, context, callback) {
-const token = “pate5XEuSM0tLC1vz.d80f1e0ea490adce86e675f8ae6c272b052b9ca320b060022343878da98eaefa”;
-const base = “apphfGx38pLpWBELJ”;
-const table = “tblpYsn8L3dLZ7Ete”;
-
-const options = {
-hostname: “api.airtable.com”,
-path: “/v0/” + base + “/” + table,
-method: “GET”,
-headers: {
-“Authorization”: “Bearer “ + token
-}
-};
-
-var body = “”;
-
-https.request(options, function(res) {
-res.on(“data”, function(chunk) {
-body += chunk;
-});
-res.on(“end”, function() {
-callback(null, {
-statusCode: 200,
-headers: {
-“Access-Control-Allow-Origin”: “*”,
-“Content-Type”: “application/json”
-},
-body: body
-});
-});
-}).on(“error”, function(err) {
-callback(null, {
-statusCode: 500,
-body: JSON.stringify({ error: err.message })
-});
-}).end();
+  var options = {
+    hostname: "api.airtable.com",
+    path: "/v0/apphfGx38pLpWBELJ/tblpYsn8L3dLZ7Ete",
+    method: "GET",
+    headers: {
+      Authorization: "Bearer pate5XEuSM0tLC1vz.d80f1e0ea490adce86e675f8ae6c272b052b9ca320b060022343878da98eaefa"
+    }
+  };
+  var body = "";
+  https.request(options, function(res) {
+    res.on("data", function(c) { body += c; });
+    res.on("end", function() {
+      callback(null, {
+        statusCode: 200,
+        headers: { "Access-Control-Allow-Origin": "*" },
+        body: body
+      });
+    });
+  }).on("error", function(e) {
+    callback(null, { statusCode: 500, body: e.message });
+  }).end();
 };
