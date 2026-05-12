673526628bd0a11ef0ce9318b204fe85fac768f8
byflore <byflorestudio.1@gmail.com>
2026-05-12 14:16:39 +0800

    Update product.js

--- api/product.js
+++ api/product.js
@@ -1,35 +1,24 @@
-var https = require("https");
-exports.handler = function(a, b, cb) {
-  var body = "";
-  var req = https.request({
-    hostname: "api.airtable.com",
-    path: "/v0/apphfGx38pLpWBELJ/tblpYsn8L3dLZ7Ete",
-    headers: {
-      Authorization: "Bearer pate5XEuSM0tLC1vz.d80f1e0ea490adce86e675f8ae6c272b052b9ca320b060022343878da98eaefa"
-    }
-  }, function(res) {
-    res.on("data", function(chunk) {
-      body = body + chunk;
-    });
-    res.on("end", function() {
-      cb(null, {
-        statusCode: 200,
-        headers: {
-          "Access-Control-Allow-Origin": "*"
-        },
-        body: body
-      });
-    });
-  });
-  req.on("error", function(e) {
-    cb(null, {
-      statusCode: 500,
-      body: e.message
-    });
-  });
-  req.end();
+var https = require(“https”);
+module.exports = function(req, res) {
+var body = “”;
+var request = https.request({
+hostname: “api.airtable.com”,
+path: “/v0/apphfGx38pLpWBELJ/tblpYsn8L3dLZ7Ete”,
+headers: {
+Authorization: “Bearer pate5XEuSM0tLC1vz.d80f1e0ea490adce86e675f8ae6c272b052b9ca320b060022343878da98eaefa”
+}
+}, function(response) {
+response.on(“data”, function(chunk) {
+body = body + chunk;
+});
+response.on(“end”, function() {
+res.setHeader(“Access-Control-Allow-Origin”, “*”);
+res.setHeader(“Content-Type”, “application/json”);
+res.status(200).send(body);
+});
+});
+request.on(“error”, function(e) {
+res.status(500).send(e.message);
+});
+request.end();
 };
-
-
-
-
