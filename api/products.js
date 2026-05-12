eb90ed6914ee4d513b2fec74e3c8170d5ba3b044
byflore <byflorestudio.1@gmail.com>
2026-05-12 14:26:06 +0800

    Update products.js

--- api/products.js
+++ api/products.js
@@ -1,24 +1,18 @@
 var https = require(“https”);
 module.exports = function(req, res) {
+res.setHeader(“Access-Control-Allow-Origin”, “*”);
 var body = “”;
-var request = https.request({
+https.get(
+{
 hostname: “api.airtable.com”,
 path: “/v0/apphfGx38pLpWBELJ/tblpYsn8L3dLZ7Ete”,
 headers: {
 Authorization: “Bearer pate5XEuSM0tLC1vz.d80f1e0ea490adce86e675f8ae6c272b052b9ca320b060022343878da98eaefa”
 }
-}, function(response) {
-response.on(“data”, function(chunk) {
-body = body + chunk;
-});
-response.on(“end”, function() {
-res.setHeader(“Access-Control-Allow-Origin”, “*”);
-res.setHeader(“Content-Type”, “application/json”);
-res.status(200).send(body);
-});
-});
-request.on(“error”, function(e) {
-res.status(500).send(e.message);
-});
-request.end();
+},
+function(r) {
+r.on(“data”, function(d) { body += d; });
+r.on(“end”, function() { res.end(body); });
+}
+).on(“error”, function(e) { res.end(JSON.stringify({error: e.message})); });
 };
