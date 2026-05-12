3980e73a89cfd181da14cd19e6a59d98caf28d23
byflore <byflorestudio.1@gmail.com>
2026-05-12 09:52:04 +0800

    Update products.js

--- netlify/functions/products.js
+++ netlify/functions/products.js
@@ -1,14 +1,10 @@
-var https = require("https");
-exports.handler = function(e, c, cb) {
-  var b = "";
-  https.request({
-    hostname: "api.airtable.com",
-    path: "/v0/apphfGx38pLpWBELJ/tblpYsn8L3dLZ7Ete",
-    headers: { Authorization: "Bearer pate5XEuSM0tLC1vz.d80f1e0ea490adce86e675f8ae6c272b052b9ca320b060022343878da98eaefa" }
-  }, function(r) {
-    r.on("data", function(d) { b += d; });
-    r.on("end", function() {
-      var j = JSON.parse(b);
-      var out = (j.records || []).map(function(rec) {
-        var f = rec.fields;
-        var imgs = f.image ? f.image.map(function(​​​​​​​​​​​​​​​​
+var h=require("https"),t="pate5XEuSM0tLC1vz.d80f1e0ea490adce86e675f8ae6c272b052b9ca320b060022343878da98eaefa";
+exports.handler=function(q,c,done){
+var b="";
+h.request({hostname:"api.airtable.com",path:"/v0/apphfGx38pLpWBELJ/tblpYsn8L3dLZ7Ete",headers:{Authorization:"Bearer "+t}},function(r){
+r.on("data",function(d){b+=d;});
+r.on("end",function(){
+done(null,{statusCode:200,headers:{"Access-Control-Allow-Origin":"*"},body:b});
+});
+}).on("error",function(e){done(null,{statusCode:500,body:e.message});}).end();
+};
